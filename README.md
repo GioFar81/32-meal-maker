a8a5800080a2675b934ea9a0a825feac913a85ef
GioFar <farigiovanni@gmail.com>
2022-06-06 03:11:39 +0100

    Version 04, now the app will print the daily meals for the entire week, combining the week-day name.

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # Project 32 from the Full Stack Course, Meal Maker.
 
-I created the app following the initial outline from the course, and then added a way to randomize the meals of the day with their
-respective values. To achieve that, I created an array for the meal options, an array for the price options, and a variable that creates a
-random number of the length of the meals array so that it stays updated automatically as I add or remove items from the list.
+## Version 01.
+
+This is the initial state of the app, as the course project requests. The app uses getters and setters to make sure that the values for the
+variables are correct, and then print a log with the value entered.
+
+## Version 02.
+
+I added a way to randomize the meals of the day with their respective prices. To achieve that, I created an array for the meal options, an
+array for the price options, and a variable that creates a random number of the length of the meal array so that it stays updated
+automatically as I add or remove items from the list.
 
 Since I want each meal to have the same price all the time they get selected, I used a single randomizer, so that each meal and price are
-kept coupled by their index in the array, meals index 0 need to be used with price index 0, and so forth. To make it easier to track, I
-added comments in the price array so that the name of the meal correlated it's next to it.
+kept coupled by their index in the array, meal index 0 need to be used with price index 0, and so forth.
+
+## Version 03.
+
+To make it easier to track, I added comments all over the code and in the price array so that the name of the meal correlated it's next to
+it. I've also added the README file (I wish I added it earlier, so that each git version had the proper update in here).
+
+## Version 04.
+
+In this version, I first planned to have a random daily meal that was able to be different for 7 times(days of the week). To be able to
+achieve this, I looked for ways to remove the selected item from the list and then, when the list was empty, reset it, but I couldn't find
+the way, so I ended up having the app creating a weekly list for the meals of the day, the log shows the weekly list combined with the
+appropriate day of the week, and it also shows a warning message if the number of meals are more or less than 7 stating how many meals need
+to be added or removed. 
--- mealMaker.js
+++ mealMaker.js
@@ -4,37 +4,71 @@ const menu = {
     //Let set the new value of meal only if it's a string.
     set meal(mealToCheck) {
         if (typeof mealToCheck === "string") {
-            return this._meal = mealToCheck;
+            return (this._meal = mealToCheck);
         }
     },
     //Let set the new value of price only if it's a number.
     set price(priceToCheck) {
         if (typeof priceToCheck === "number") {
-            return this._price = priceToCheck;
+            return (this._price = priceToCheck);
         }
     },
     //If both setters are true, then return a message using them, otherwise return message saying the values are wrong.
     get todaysSpecial() {
         if (this._meal && this._price) {
-            return `Today's Special is ${this._meal}, for just ${this._price}£!!`;
+            return `'s Special is ${this._meal}, for just ${this._price}£!!`;
         } else {
             return "Meal and Price wasn't entered correctly!!";
         }
     }
 };
 
-//Arrays for the meal options and respective prices, also a constant to get a random number in the array range.
-const meals = ["Pizza", "Steak", "Pie", "Roast", "Moussaka", "Lasagne", "Tacos"];
-const prices = [/*Pizza*/9, /*Steak*/13, /*Pie*/11, /*Roast*/14, /*Moussaka*/9, /*Lasagne*/10, /*Tacos*/9];
-const random = Math.floor(Math.random() * meals.length);
+//Array for the meal options and respective prices.
+let meals = [
+    {/*1*/name: "Pizza", price: 9},
+    {/*2*/name: "Steak", price: 13},
+    {/*3*/name: "Pie", price: 11},
+    {/*4*/name: "Roast", price: 14},
+    {/*5*/name: "Moussaka", price: 9},
+    {/*6*/name: "Lasagne", price: 10},
+    {/*7*/name: "Tacos", price: 9}
+];
 
-//Assigns a new random value from the arrays. I used a single randomizer so that you can combine a plate to its price by the index number.
-menu.meal = meals[random];
-menu.price = prices[random];
+//Shuffle function, creates two variables.
+const shuffle = (array) => {
+    let currentIndex = array.length,
+        randomIndex;
+    //The function starts from the array length and ends when reaches 0
+    while (currentIndex !== 0) {
+        //Assigns a random number to the variable, and each time it runs, it lowers the max number by 1.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        //It swaps the current array element, with the one equivalent to the random number index.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex]
+        ];
+    }
+    return array;
+}
 
-//Check if the number of items in the meals and prices array it's equal, and if it is, creates the menu of the day string.
-if (meals.length === prices.length) {
-    console.log(menu.todaysSpecial);
-} else {
-    console.log("The number of prices and meals don't match!!");
+meals = shuffle(meals);
+const dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+//I've created a for loop to go through the new shuffled array.
+for (let i = 0; i < meals.length; i++) {
+    //In here I'm saying that if the number of meals is equal to the week days, the weekly menu will be printed.
+    if (meals.length === dayOfTheWeek.length) {
+        menu.meal = meals[i].name;
+        menu.price = meals[i].price;
+        console.log(dayOfTheWeek[i] + menu.todaysSpecial);
+        //If the meals are less, it will print how many need to be added.
+    } else if (meals.length < dayOfTheWeek.length) {
+        console.log(`You need to add ${Math.abs(meals.length - dayOfTheWeek.length)} meal(s) to the list!!!`)
+        break;
+        //If the meals are more, than it will print how many need to be removed.
+    } else if (meals.length > dayOfTheWeek.length) {
+        console.log(`You need to remove ${Math.abs(meals.length - dayOfTheWeek.length)} meal(s) from the list!!!`)
+        break;
+    }
 }
