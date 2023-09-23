d4185185982952134c5b8505bc15584cd8b36f3e
GioFar <60364147+GioFar81@users.noreply.github.com>
2023-09-24 00:22:10 +0100

    Version 07, now the app will also shuffle the four weekly menus.

--- README.md
+++ README.md
@@ -2,37 +2,43 @@
 
 ## Version 01.
 
-This is the initial state of the app, as the course project requests. The app uses getters and setters to make sure that the values for the
-variables are correct, and then print a log with the value entered.
+This is the initial state of the app, as the course project requests. The app uses getters and setters to make sure that
+the values for the variables are correct, and then print a log with the value entered.
 
 ## Version 02.
 
-I added a way to randomize the meals of the day with their respective prices. To achieve that, I created an array for the meal options, an
-array for the price options, and a variable that creates a random number of the length of the meal array so that it stays updated
-automatically as I add or remove items from the list.
+I added a way to randomise the meals of the day with their respective prices. To achieve that, I created an array for
+the meal options, an array for the price options, and a variable that creates a random number of the lengths of the meal
+array so that it stays updated automatically as I add or remove items from the list.
 
-Since I want each meal to have the same price all the time they get selected, I used a single randomizer, so that each meal and price are
-kept coupled by their index in the array, meal index 0 need to be used with price index 0, and so forth.
+Since I want each meal to have the same price all the time they get selected, I used a single randomiser, so that each
+meal and price are kept coupled by their index in the array, meal index 0 need to be used with price index 0, and so
+forth.
 
 ## Version 03.
 
-To make it easier to track, I added comments all over the code and in the price array so that the name of the meal correlated it's next to
-it. I've also added the README file (I wish I added it earlier, so that each git version had the proper update in here).
+To make it easier to track, I added comments all over the code and in the price array so that the name of the meal
+correlated it next to it. I've also added the README file (I wish I added it earlier, so that each git version had the
+proper update in here).
 
 ## Version 04.
 
-In this version, I first planned to have a random daily meal that was able to be different for 7 times(days of the week). To be able to
-achieve this, I looked for ways to remove the selected item from the list and then, when the list was empty, reset it, but I couldn't find
-the way, so I ended up having the app creating a weekly list for the meals of the day, the log shows the weekly list combined with the
-appropriate day of the week, and it also shows a warning message if the number of meals are more or less than 7 stating how many meals need
-to be added or removed.
+In this version, I first planned to have a random daily meal that was able to be different for seven times (days of the
+week). To be able to achieve this, I looked for ways to remove the selected item from the list and then, when the list
+was empty, reset it, but I couldn't find the way, so I ended up having the app creating a weekly list for the meals of
+the day, the log shows the weekly list combined with the appropriate day of the week, and it also shows a warning
+message if the number of meals are more or less than 7 stating how many meals need to be added or removed.
 
 ## Version 05.
 
-Added a variable with the week list, and then changed the loop so that the same weekly menu gets shuffled and logged 4 times, right below
-the week number.
+Added a variable with the week list, and then changed the loop so that the same weekly menu gets shuffled and logged
+four times, right below the week number.
 
 ## Version 06.
 
-Added 3 more arrays with the weekly menu list, for a total of 4. Now the app takes each of the 4 arrays and shuffles them before logging the
-result.
+Added three more arrays with the weekly menu list, for a total of 4. Now the app takes each of the four arrays and
+shuffles them before logging the result.
+
+## Version 07.
+
+Added a function so that the app will shuffle the four arrays representing the weeks, and not just the content.
--- mealMaker.js
+++ mealMaker.js
@@ -13,52 +13,53 @@ const menu = {
             return (this._price = priceToCheck);
         }
     },
-    //If both setters are true, then return a message using them, otherwise return message saying the values are wrong.
+    //If both setters are true, then return a message using them,
+    // otherwise return a message saying the values are wrong.
     get todaySpecial() {
         if (this._meal && this._price) {
             return `'s Special is ${this._meal}, for just ${this._price}£!!`;
         } else {
             return "Meal and Price wasn't entered correctly!!";
         }
-    }
+    },
 };
 
 //Array for the meal options and respective prices.
 let meals0 = [
-    {/*1*/name: "Pizza", price: 9},
-    {/*2*/name: "Steak", price: 11},
-    {/*3*/name: "Pie", price: 9},
-    {/*4*/name: "Roast", price: 12},
-    {/*5*/name: "Moussaka", price: 9},
-    {/*6*/name: "Lasagne", price: 8},
-    {/*7*/name: "Tacos", price: 8}
+    {/*1*/name: "Pizza", price: 9 },
+    {/*2*/name: "Steak", price: 11 },
+    {/*3*/name: "Pie", price: 9 },
+    {/*4*/name: "Roast", price: 12 },
+    {/*5*/name: "Moussaka", price: 9 },
+    {/*6*/name: "Lasagne", price: 8 },
+    {/*7*/name: "Tacos", price: 8 },
 ];
 let meals1 = [
-    {/*1*/name: "Omelette", price: 7},
-    {/*2*/name: "Kebab", price: 10},
-    {/*3*/name: "Ratatouille", price: 8},
-    {/*4*/name: "Quiche", price: 8},
-    {/*5*/name: "Burgers", price: 11},
-    {/*6*/name: "Hot-Dogs", price: 8},
-    {/*7*/name: "Burritos", price: 9}
+    {/*1*/name: "Omelette", price: 7 },
+    {/*2*/name: "Kebab", price: 10 },
+    {/*3*/name: "Ratatouille", price: 8 },
+    {/*4*/name: "Quiche", price: 8 },
+    {/*5*/name: "Burgers", price: 11 },
+    {/*6*/name: "Hot-Dogs", price: 8 },
+    {/*7*/name: "Burritos", price: 9 },
 ];
 let meals2 = [
-    {/*1*/name: "Bourguignon", price: 10},
-    {/*2*/name: "Stew", price: 11},
-    {/*3*/name: "Fish&Chips", price: 12},
-    {/*4*/name: "Fried Chicken", price: 7},
-    {/*5*/name: "Cannelloni", price: 10},
-    {/*6*/name: "Crepes", price: 8},
-    {/*7*/name: "Gyros", price: 10}
+    {/*1*/name: "Bourguignon", price: 10 },
+    {/*2*/name: "Stew", price: 11 },
+    {/*3*/name: "Fish&Chips", price: 12 },
+    {/*4*/name: "Fried Chicken", price: 7 },
+    {/*5*/name: "Cannelloni", price: 10 },
+    {/*6*/name: "Crepes", price: 8 },
+    {/*7*/name: "Gyros", price: 10 },
 ];
 let meals3 = [
-    {/*1*/name: "Greek Salad", price: 9},
-    {/*2*/name: "Seafood", price: 13},
-    {/*3*/name: "Falafel", price: 8},
-    {/*4*/name: "Wraps", price: 10},
-    {/*5*/name: "Souvlaki", price: 11},
-    {/*6*/name: "Shawarma", price: 10},
-    {/*7*/name: "Quesadilla", price: 9},
+    {/*1*/name: "Greek Salad", price: 9 },
+    {/*2*/name: "Seafood", price: 13 },
+    {/*3*/name: "Falafel", price: 8 },
+    {/*4*/name: "Wraps", price: 10 },
+    {/*5*/name: "Souvlaki", price: 11 },
+    {/*6*/name: "Shawarma", price: 10 },
+    {/*7*/name: "Quesadilla", price: 9 },
 ];
 
 
@@ -72,36 +73,60 @@ const shuffle = (array) => {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
         //It swaps the current array element, with the one equivalent to the random number index.
-        [array[currentIndex], array[randomIndex]] = [
+        [
+            array[currentIndex],
             array[randomIndex],
-            array[currentIndex]
+        ] = [
+            array[randomIndex],
+            array[currentIndex],
         ];
     }
     return array;
-}
+};
 
-let week = ["Week 1", "Week 2", "Week 3", "Week 4"];
-let all_meals = [meals0, meals1, meals2, meals3];
-const dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let week = [
+    "Week 1",
+    "Week 2",
+    "Week 3",
+    "Week 4",
+];
+let all_meals = [
+    meals0,
+    meals1,
+    meals2,
+    meals3,
+];
+const dayOfTheWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+
+// Shuffle the order of meal lists
+shuffle(all_meals);
 
 //I've created a for loop to go through the new shuffled array.
-
 for (let w = 0; w < week.length; w++) {
-    console.log("\n\n" + week[w] + "\n")
+    console.log("\n\n" + week[w] + "\n");
     let week_meals = shuffle(all_meals[w]);
+
     for (let i = 0; i < all_meals[w].length; i++) {
-        //In here I'm saying that if the number of meals is equal to the week days, the weekly menu will be printed.
+        //In here, I'm saying that if the number of meals is equal to the week days, the weekly menu will be printed.
         if (week_meals.length === dayOfTheWeek.length) {
             menu.meal = week_meals[i].name;
             menu.price = week_meals[i].price;
             console.log(dayOfTheWeek[i] + menu.todaySpecial);
             //If the meals are less, it will print how many need to be added.
         } else if (week_meals.length < dayOfTheWeek.length) {
-            console.log(`You need to add ${Math.abs(week_meals.length - dayOfTheWeek.length)} meal(s) to the list!!!`)
+            console.log(`You need to add ${Math.abs(week_meals.length - dayOfTheWeek.length)} meal(s) to the list!!!`);
             break;
             //If the meals are more, than it will print how many need to be removed.
         } else if (week_meals.length > dayOfTheWeek.length) {
-            console.log(`You need to remove ${Math.abs(week_meals.length - dayOfTheWeek.length)} meal(s) from the list!!!`)
+            console.log(`You need to remove ${Math.abs(week_meals.length - dayOfTheWeek.length)} meal(s) from the list!!!`);
             break;
         }
     }
